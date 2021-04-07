04ccfccc975c2bbdb69bf742b576ed5776141981
Minigrim0 <grimauflorent@gmail.com>
2021-04-07 15:04:27 +0200

    [Upd] gitignore + parsing ijk

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import sys
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 3:
     print("Ouech faut des parametres fdp")
+
+
+filepath, p = sys.argv[1:]
+with open(filepath) as f:
+    a = f.readline()
+    i, j, k = a.split()
+    print(i, j, k)
